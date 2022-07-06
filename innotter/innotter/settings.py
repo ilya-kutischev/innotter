7be74986e2e50492ca77d22f1b7ce31e3a566179
root <kutischev10@gmail.com>
2022-07-06 16:45:20 +0300

    "test"

--- innotter/innotter/settings.py
+++ innotter/innotter/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True == (os.getenv('DEBUG'))
+DEBUG = "True" == (os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
